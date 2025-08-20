5709889708d3a9a406e4f682dd3bbff54c8eace7
saeidha <saeed221373@yahoo.com>
2025-08-20 19:22:50 +0330

    add bartch process

--- batchTransfer.js
+++ batchTransfer.js
@@ -1,3 +1,7 @@
 
 
 // ======= FILE 2: batchTransfer.js =======
+
+const { ethers } = require('ethers');
+const fs = require('fs');
+require('dotenv').config(); // Load environment variables from .env file
